2afe9ace8d8529d3b04095840ae013d7686aa168
Clementine <clementine11@vk.com>
2022-05-22 15:16:30 +0300

    Update tty.c

--- kernel/src/io/tty.c
+++ kernel/src/io/tty.c
@@ -228,6 +228,13 @@ void tty_puthex(uint32_t i) {
     tty_putchar(hex[n]);
 }
 
+void putcharpos(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y)
+{
+     uint16_t attrib = (backcolour << 4) | (forecolour & 0x0F);
+     volatile uint16_t * where;
+     where = (volatile uint16_t *)0xB8000 + (y * 80 + x) ;
+     *where = c | (attrib << 8);
+}
 
 /*
     tty_print32_t - строгий, форматированный вывод
