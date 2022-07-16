7bc8c532abd22349ddab58942c4343e56253ed4e
Aren Elchinyn <slizen.can@gmail.com>
2022-07-16 17:21:20 +0300

    Исправлена сборка

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
-import os, shutil, sys, tarfile, os.path
+import os, shutil, sys, tarfile, time
 
 
 CC = "i686-elf-gcc -g -w -ffreestanding -I kernel/include/ -c"
 
 
-def build_all():
+def build_kernel():
+    start_time = time.time()
     print("Building kernel")
 
     SRC_TARGETS = []
@@ -25,7 +26,6 @@ def build_all():
     for i in range(0, len(SRC_TARGETS)):
         os.system(f"{CC} {SRC_TARGETS[i]} -o {BIN_TARGETS[i]}")
         
-        print(i, SRC_TARGETS[i], BIN_TARGETS[i])
 
     # Получаем список файлов в переменную files
     files = os.listdir("bin/kernel/")
@@ -38,47 +38,77 @@ def build_all():
         OBJ += f"bin/kernel/{i} "
 
     os.system("i686-elf-gcc -T kernel/link.ld -nostdlib -lgcc -o isodir/boot/kernel.elf " + OBJ)
+    print(f"Build end at: {time.time() - start_time}")
 
+
+
+def build_apps():
+    os.chdir("apps/")
+    os.system("python build.py")
+
+    shutil.rmtree("../initrd/apps", ignore_errors=True)
+    shutil.copytree("../bin/apps", "../initrd/apps")
+
+    os.chdir("../initrd")
+        
+
+    with tarfile.open("../isodir/boot/initrd.tar", "w") as tar:
+        for i in os.listdir():
+            tar.add(i)
+    os.chdir("../")
+
+
+def create_iso():
+    print("Creating ISO")
+    start_time = time.time()
+
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system("grub-mkrescue -o \"SynapseOS.iso\" isodir/ -V SynapseOS")
+    else:
+        os.system("ubuntu run grub-mkrescue -o \"SynapseOS.iso\" isodir/ -V SynapseOS ")
+    
+    print(f"Build end at: {time.time() - start_time}")
+
+
+def run_qemu():
+    if os.path.exists("ata.vhd"):
+        pass
+    else:
+        os.system("qemu-img create -f raw ata.vhd 32M")
+    
+    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw all -m 16" \
+        " -netdev socket,id=n0,listen=:2030 -device rtl8139,netdev=n0,mac=11:11:11:11:11:11 " \
+        " -cdrom SynapseOS.iso -hda ata.vhd -serial  file:Qemu.log"
+        
+    os.system(
+        qemu_command
+        )
 
 
 if __name__ == "__main__":
     try:
-        build_all()
+        start_time = time.time()
 
-        if os.path.exists("ata.vhd"):
-            pass
+        # Стандартная сборка
+        
+        if len(sys.argv) == 1:
+            build_kernel()
+            build_apps()
+            create_iso()
+            run_qemu()
         else:
-            os.system("qemu-img create -f raw ata.vhd 32M")
-        
+            for i in range(1, len(sys.argv)):
+                if sys.argv[i] == "kernel":
+                    build_kernel()
+                elif sys.argv[i] == "apps":
+                    build_apps()
+                elif sys.argv[i] == "iso":
+                    create_iso()
+                elif sys.argv[i] == "run":
+                    run_qemu()
+                else:
+                    print(f"Ошибка, неизвестный аргумент: {sys.argv[i]}")
+        print(f"Конец: {time.time() - start_time}")
 
-        os.chdir("apps/")
-        os.system("python build.py")
-
-        shutil.rmtree("../initrd/apps", ignore_errors=True)
-        shutil.copytree("../bin/apps", "../initrd/apps")
-
-        os.chdir("../initrd")
-        
-
-        with tarfile.open("../isodir/boot/initrd.tar", "w") as tar:
-            for i in os.listdir():
-                tar.add(i)
-        
-        os.chdir("../")
-        
-        print("Creating ISO")
-
-        if sys.platform == "linux" or sys.platform == "linux2":
-            os.system("grub-mkrescue -o \"SynapseOS.iso\" isodir/ -V SynapseOS")
-        else:
-            os.system("ubuntu run grub-mkrescue -o \"SynapseOS.iso\" isodir/ -V SynapseOS ")
-
-        qemu_command = "qemu-system-i386 -name SynapseOS -soundhw all -m 16" \
-            " -netdev socket,id=n0,listen=:2030 -device rtl8139,netdev=n0,mac=11:11:11:11:11:11 " \
-            " -cdrom SynapseOS.iso -hda ata.vhd -serial  file:Qemu.log"
-        
-        os.system(
-            qemu_command
-            )
     except Exception as E:
         print(E)
