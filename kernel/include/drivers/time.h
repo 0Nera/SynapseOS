17f4bbb1b0a5c72df9966ff6b3f54eaab13967f1
Никита <48570109+pimnik98@users.noreply.github.com>
2022-07-23 21:10:53 +0300

    TUI v0.0.1
    * Добавлена функция для обрезки строк: void substr(char* dest, char* source, int from, int length) -> string.c
    * Добавлена функция для склейки строк с указанием длины: char *strncpy(char *dest, const char *src, size_t n) -> string.c
    * Добавлена функция для получения размера экрана uint32_t getWidthScreen() и uint32_tgetHeightScreen() -> tty.c
    * Добавлена функция для установки позиции курсора (который выводит текст) void setPosX() и void setPosY() -> tty.c
    * Добавлены функции для вывода цветного текста tty_putchar_color(char c,uint32_t txColor,uint32_t bgColor) и void tty_puts_color(const char c[],uint32_t txColor,uint32_t bgColor) -> tty.c
    * В консольное приложение добавлены функции для выключения ядра (shutdown) / перезагрузки (reboot) / отчистки экрана (cls) -> shell.c
    * Добавлена демо-версия TUI, но так как функционала сейчас не несет, через 10 сек перекидывает в консольное приложение -> tui.c
    * В TUI есть функция которая используется для детекта типа расширения монитора (QVGA,SIF(MPEG1 SIF),WQVGA и т.д) -> tui.c
    * Функция void drawRect(int x,int y,int w, int h,int color) - рисует прямоугольник -> tui.c
    * Функция void drawRectLine(int x,int y,int w, int h,int color, int c) - рисует внутри прямоугольника узор из символа
    * Добавлена инициализация tui() перед shell() -> kernel.c
    * Добавлен инклуд tui.h -> kernel.h
    * Импортированы старые исправления: добавлен sleep() и перенесены функции из ports.c в system.c

--- kernel/include/drivers/time.h
+++ kernel/include/drivers/time.h
@@ -1,8 +1,6 @@
 #pragma once
-
-#pragma once
-
 void init_task_manager();
 void timer_install();
 void timer_set_frequency();
 int32_t timer_get_ticks();
+void sleep(uint16_t delay);
---
+++ kernel/include/io/tui.h
@@ -0,0 +1,2 @@
+#pragma once
+bool tui();
