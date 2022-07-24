dbd281ba674e27827836567e498f5f1e0fcdf083
DomamaN202 <vip.domaman@mail.ru>
2022-07-24 21:07:47 +0700

    4 стандартных библиотеки

---
+++ kernel/include/libk/stdalign.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// #define __alignas_is_defined 1 // TODO: need test
+#define __alignof_is_defined 1
+
+#ifndef __cplusplus
+// #define alignas _Alignas // TODO: need test
+#define alignof _Alignof
+#endif
