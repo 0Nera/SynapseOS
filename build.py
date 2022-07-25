d5daa0d217bb18aef711ed03445d02aed5be8459
NDRAEY <andrejpavlenko666@gmail.com>
2022-07-25 18:52:38 +0900

    Added './melody' program

--- build.py
+++ build.py
@@ -143,7 +143,7 @@ def run_qemu():
     else:
         os.system("qemu-img create -f raw ata.vhd 32M")
     
-    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -device ac97 -device sb16 -m 32" \
+    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -m 32" \
         " -netdev socket,id=n0,listen=:2030 -device rtl8139,netdev=n0,mac=11:11:11:11:11:11 " \
         " -cdrom SynapseOS.iso -hda ata.vhd -serial  file:Qemu.log"
         
@@ -155,7 +155,7 @@ def run_kvm():
     if not os.path.exists("ata.vhd"):
         os.system("qemu-img create -f raw ata.vhd 32M")
     
-    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -device ac97 -device sb16 -m 32" \
+    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -m 32" \
         " -netdev socket,id=n0,listen=:2030 -device rtl8139,netdev=n0,mac=11:11:11:11:11:11 " \
         " -cdrom SynapseOS.iso -hda ata.vhd -serial  file:Qemu.log -accel kvm"
         
@@ -168,7 +168,7 @@ def run_qemu_debug():
     else:
         os.system("qemu-img create -f raw ata.vhd 32M")
     
-    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -device ac97 -device sb16 -m 32" \
+    qemu_command = "qemu-system-i386 -name SynapseOS -soundhw pcspk -m 32" \
         " -netdev socket,id=n0,listen=:2030 -device rtl8139,netdev=n0,mac=11:11:11:11:11:11 " \
         " -cdrom SynapseOS.iso -hda ata.vhd -serial  file:Qemu.log" 
     print("gdb kernel.elf -ex target remote localhost:1234")
