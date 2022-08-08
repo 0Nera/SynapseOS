d95c85fdbb5add5832f0a938ad6cf21933a9f346
Gagarinten <71782778+noverd@users.noreply.github.com>
2022-08-08 09:31:54 +0300

    Fix

--- apps/include/stddef.h
+++ apps/include/stddef.h
@@ -2,11 +2,7 @@
 #define _SIZE_T
 typedef unsigned int size_t;
 #endif
-
 #undef NULL
 #define NULL ((void *)0)
-
 #undef offsetof
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-
-#endif
