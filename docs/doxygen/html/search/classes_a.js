cd5c34a4856403262dd95633ab3a7523fe031d2d
NDRAEY (Locally Zangoose) <andrejpavlenko666@gmail.com>
2022-08-21 01:46:57 +0900

    DRUNKEN DOCS!

--- docs/doxygen/html/search/classes_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pci_5fdev_135',['pci_dev',['../d7/dfa/unionpci__dev.html',1,'']]]
-];
--- docs/doxygen/html/search/classes_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['regs_136',['regs',['../d3/da0/structregs.html',1,'']]],
-  ['rgba_5fstruct_137',['rgba_struct',['../db/d9d/structrgba__struct.html',1,'']]],
-  ['rtl8139_5fdev_138',['RTL8139_dev',['../da/dd2/struct_r_t_l8139__dev.html',1,'']]]
-];
