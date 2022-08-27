58d9d208345b2ad12ea406baa365b9196fe0eec8
Github <github@example.com>
2022-08-27 12:16:51 +0000

    документация: Обновление документации с помощью doxygen [Автосборка]

--- docs/doxygen/html/d1/daa/structvfs__filesystem__handles__t.js
+++ docs/doxygen/html/d1/daa/structvfs__filesystem__handles__t.js
@@ -4,10 +4,10 @@ var structvfs__filesystem__handles__t =
     [ "get_size", "d1/daa/structvfs__filesystem__handles__t.html#a0244b3b1e48cabfc5e474b43da17672f", null ],
     [ "is_dir", "d1/daa/structvfs__filesystem__handles__t.html#aca9c552a1e164a008e9e213aae93697b", null ],
     [ "ls", "d1/daa/structvfs__filesystem__handles__t.html#a4442dba9e4b617415d099a1ed42b485b", null ],
-    [ "mkdir", "d1/daa/structvfs__filesystem__handles__t.html#ac2e8b26619dbc65123001fbc6a1ed9b7", null ],
-    [ "mkfile", "d1/daa/structvfs__filesystem__handles__t.html#a7b7c44ce92902c738893e3e321be070c", null ],
-    [ "read", "d1/daa/structvfs__filesystem__handles__t.html#a77db303ee0153a624940b89275c834cd", null ],
+    [ "mkdir", "d1/daa/structvfs__filesystem__handles__t.html#a20b96464c0c0c116c3a0b6439b1d54d3", null ],
+    [ "mkfile", "d1/daa/structvfs__filesystem__handles__t.html#a2b7259ce7ac4225d9e072cd05a01eb80", null ],
+    [ "read", "d1/daa/structvfs__filesystem__handles__t.html#aae2e0c70d8777a5c920cc8a4a0b597a1", null ],
     [ "readdir", "d1/daa/structvfs__filesystem__handles__t.html#af770f22a898c22d328d2a772a2109cd8", null ],
     [ "rm", "d1/daa/structvfs__filesystem__handles__t.html#a40895da202c6e9ab5bb3f56250de4ecb", null ],
-    [ "write", "d1/daa/structvfs__filesystem__handles__t.html#ab1182a34bf5789e88cbcf4a4f2ab6dcb", null ]
+    [ "write", "d1/daa/structvfs__filesystem__handles__t.html#a08d387ee1b3fe3d023f4c9e1ebd9c652", null ]
 ];
